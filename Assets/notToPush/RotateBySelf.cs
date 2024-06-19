2a20cff5ba7a91d09e9413955110acd6de00d9ef
MikeTCN <nooble898@gmail.com>
2024-06-20 00:04:37 +0100

    not to push allowed to push

---
+++ Assets/notToPush/RotateBySelf.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a794ffa3d813d538dfca7d49a351e1ba5d1afe13239b2fdc99884463a63756e9
+size 319
---
+++ Assets/notToPush/RotateByTA.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c7143ecdf178e606434137135601f1d191ced66efb96108c4bfc9693ec192f7
+size 789
