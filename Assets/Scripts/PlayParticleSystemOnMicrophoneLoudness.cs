f30892e2e78a8e4a62d366aaaef719df1ff6d846
MikeTCN <nooble898@gmail.com>
2024-07-30 19:48:30 +0100

    Mars terrain basic function is done, polish needed

--- Assets/Scripts/PlayParticleSystemOnMicrophoneLoudness.cs
+++ Assets/Scripts/PlayParticleSystemOnMicrophoneLoudness.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,21 +8,34 @@ public class PlayParticleSystemOnMicrophoneLoudness : MonoBehaviour
     public ParticleSystem particles;
     public float loudnessSensibility = 100;
     public float threshold = 0.1f;
+    public AudioSource audioSource; // 添加AudioSource
 
     // Update is called once per frame
     void Update()
     {
         float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
 
-
         if (loudness <= threshold && particles.isPlaying)
+        {
             particles.Stop();
-        else if(loudness > threshold && particles.isStopped)
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+        else if (loudness > threshold && particles.isStopped)
+        {
             particles.Play();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
     }
 
     private void OnDisable()
     {
         particles.Stop();
+        audioSource.Stop(); // 在組件禁用時也停止AudioSource
     }
 }
