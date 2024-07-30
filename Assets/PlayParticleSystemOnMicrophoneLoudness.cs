fd2aca74bfe7ea28087cdc1a8fece16e5efea282
MikeTCN <nooble898@gmail.com>
2024-07-30 15:59:42 +0100

    script particlesystem added

--- Assets/PlayParticleSystemOnMicrophoneLoudness.cs
+++ Assets/PlayParticleSystemOnMicrophoneLoudness.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayParticleSystemOnMicrophoneLoudness : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioLoudnessDetection detector;
+    public ParticleSystem particles;
+    public float loudnessSensibility = 100;
+    public float threshold = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
+
+
+        if (loudness <= threshold && particles.isPlaying)
+            particles.Stop();
+        else if (loudness > threshold && particles.isStopped)
+            particles.Play();
+    }
+
+    private void OnDisable()
+    {
+        particles.Stop();
     }
 }
