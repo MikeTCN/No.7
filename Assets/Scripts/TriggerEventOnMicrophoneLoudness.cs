5e42c8df97fdf3c10648cbe229f410637595357b
MikeTCN <nooble898@gmail.com>
2024-08-01 00:24:08 +0100

    1

--- Assets/Scripts/TriggerEventOnMicrophoneLoudness.cs
+++ Assets/Scripts/TriggerEventOnMicrophoneLoudness.cs
@@ -11,25 +11,32 @@ public class TriggerEventOnMicrophoneLoudness : MonoBehaviour
     public AudioSource audioSource; // AudioSource component
 
     public UnityEvent customEvent; // Custom UnityEvent to trigger in response to microphone loudness
+    public UnityEvent stopEvent; // Custom UnityEvent to stop other component when triggered
+
+    private bool isStopped = false; // Flag to track if stop event has been triggered
 
     void Update()
     {
         float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
 
-        if (loudness > threshold)
+        if (loudness > threshold && !isStopped)
         {
             customEvent.Invoke(); // Invoke the custom event when loudness exceeds the threshold
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
+            stopEvent.Invoke(); // Invoke the stop event to stop other components
+            isStopped = true; // Set flag to true to indicate stop event has been triggered
         }
-        else
+        else if (loudness <= threshold && isStopped)
         {
-            if (audioSource.isPlaying)
-            {
-                audioSource.Stop();
-            }
+            isStopped = false; // Reset the flag when loudness falls below threshold
+        }
+
+        if (loudness <= threshold && audioSource.isPlaying)
+        {
+            audioSource.Stop(); // Stop audio source when loudness falls below threshold
         }
     }
 
