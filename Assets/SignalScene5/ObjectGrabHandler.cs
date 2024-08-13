9b042fb071aeede27012b337b3b845b0a892a329
Genthz <wja3721@gmail.com>
2024-08-13 21:17:55 +0100

    final scene change

--- Assets/SignalScene5/ObjectGrabHandler.cs
+++ Assets/SignalScene5/ObjectGrabHandler.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class ObjectGrabHandler : MonoBehaviour
+public class ObjectGrabController : MonoBehaviour
 {
+    public XRGrabInteractable[] objectsToMonitor; // 需要监测是否被抓握的物体
     public GameObject[] objectsToAppear; // 要出现的物体
     public Vector3[] targetScales; // 每个物体的目标缩放比例
     public float scaleSpeed = 1f; // 放大速度
-    private XRGrabInteractable grabInteractable;
 
     private void Start()
     {
-        // 获取XRGrabInteractable组件
-        grabInteractable = GetComponent<XRGrabInteractable>();
-
-        // 监听抓取事件
-        grabInteractable.selectEntered.AddListener(OnGrab);
-
         // 初始化时，将对象缩小到不可见状态
         for (int i = 0; i < objectsToAppear.Length; i++)
         {
             objectsToAppear[i].transform.localScale = Vector3.zero;
             objectsToAppear[i].SetActive(false); // 隐藏对象
         }
+
+        // 为每个需要监测的物体添加抓取事件监听器
+        foreach (XRGrabInteractable grabInteractable in objectsToMonitor)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrab);
+        }
     }
 
     // 当物体被抓住时调用
-    public void OnGrab(SelectEnterEventArgs args)
+    private void OnGrab(SelectEnterEventArgs args)
     {
         StartCoroutine(ShowAndScaleObjects());
     }
